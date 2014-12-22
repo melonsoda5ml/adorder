4d7ab6f3b5e2d49b133294b09444bb1cb72172ec
melonsoda5ml <mitsuba@sfc.wide.ad.jp>
2014-12-22 13:55:15 +0900

    とりあえずメールを送るとこまで出来た

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -26,7 +26,7 @@ class OrdersController < ApplicationController
 
   def create
 		set_order(order_params)
-		redirect_to action:'index'
+		#redirect_to action:'index'
     #respond_with(@order)
   end
 
@@ -36,7 +36,7 @@ class OrdersController < ApplicationController
     #respond_with(@order)
   end
 
-def destroy
+	def destroy
 #    @order.destroy
 		Order.find(params[:id]).destroy
 		flash[:success] = "オーダーを削除しました"
@@ -44,15 +44,33 @@ def destroy
 		redirect_to orders_path
   end
 
+	def sendmail(order)
+		@order = order
+		endpoint = 'https://outlook.office365.com/ews/Exchange.asmx'
+		user = 'mozawa@nikkeibp.co.jp'
+		pass = 'Kirei333'
+		t = render_to_string('sendmail.text.erb', collection: [@order]).to_str
+		cli = Viewpoint::EWSClient.new endpoint, user, pass
+		date_ja =  @order.release_date.strftime("%Y/%m/%d(#{%w(日 月 火 水 木 金 土)[@order.release_date.wday]})")
+
+		cli.send_message do |m|
+			m.subject = "【申込み】"+@order.media
+			m.body = t
+			m.to_recipients << 'mozawa@nikkeibp.co.jp'
+			end
+			redirect_to orders_path
+	end
+
   private
     def set_order(order_params)
 #    @order = Order.find(params[:id])
 			p order_params
 			@order = Order.new
 			@order.media = order_params[:media]
-			#@release_date = Date.civil(params[:release_date][:"date(1i)"].to_i, params[:release_date][:"date(2i)"].to_i, params[:release_date][:"date(3i)"].to_i)
-			p @release_date
-			@order.release_date = order_params[:release_date]
+			year = params[:release_date]["{:use_month_numbers=>true, :start_year=>2014, :end_year=>2014, :default=>2014, :date_separator=>\"%s\"}(1i)"].to_i
+			month = params[:release_date]["{:use_month_numbers=>true, :start_year=>2014, :end_year=>2014, :default=>2014, :date_separator=>\"%s\"}(2i)"].to_i
+			date = params[:release_date]["{:use_month_numbers=>true, :start_year=>2014, :end_year=>2014, :default=>2014, :date_separator=>\"%s\"}(3i)"].to_i
+			@order.release_date = Date.new year, month, date
 			@order.client = order_params[:client]
 			@order.agent = order_params[:agent]
 			@order.space = order_params[:space]
@@ -63,16 +81,19 @@ def destroy
 			@order.user_id = current_user.id
 			@order.save
 			#UserMailer.sendmail(@order).deliver
-
+			sendmail(@order)
+=begin
 			endpoint = 'https://outlook.office365.com/ews/Exchange.asmx'
 			user = 'mozawa@nikkeibp.co.jp'
 			pass = 'Kirei333'
 			cli = Viewpoint::EWSClient.new endpoint, user, pass
+			date_ja =  @order.release_date.strftime("%Y/%m/%d(#{%w(日 月 火 水 木 金 土)[@order.release_date.wday]})")
 			cli.send_message do |m|
 				m.subject = "【申込み】"+@order.media
-				m.body    = current_user.name+"から申込みです！！！！！"
+				m.body    = date_ja+";"+current_user.name+"から申込みです！！！！！"
 				m.to_recipients << 'mozawa@nikkeibp.co.jp'
 			end
+=end
     end
 
     def order_params
