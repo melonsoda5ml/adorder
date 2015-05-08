6a0195397be7f3516d4faa2d15002d59de86554d
melonsoda5ml <mitsuba@sfc.wide.ad.jp>
2015-05-08 17:32:02 +0900

    ログアウト機能を動作するようにした／プロフィール編集部分にroleを加えた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :division, :role, :email, :password, :password_confirmation) }
 	end
+
+	def after_sign_out_path_for(resource)
+		new_user_session_path
+	end
 end
