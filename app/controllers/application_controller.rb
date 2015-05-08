3b503b1719a876b32a39fd728fbba00f056514f9
melonsoda5ml <mitsuba@sfc.wide.ad.jp>
2015-05-08 17:56:36 +0900

    application controllerでcurrent_passwordをpermitするように設定:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
 	protected
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :division, :role, :email, :password, :password_confirmation) }
+		devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :division, :role, :email, :password, :password_confirmation, :current_password) }
 	end
 
 	def after_sign_out_path_for(resource)
